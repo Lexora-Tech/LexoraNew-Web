c3233c90554e451e50d686437d640f9d0bddacae
Theekshana <145346863+ThikkaBro@users.noreply.github.com>
2025-09-22 12:02:47 +0530

    Updated edit blog

--- admin/edit_blog.php
+++ admin/edit_blog.php
@@ -26,17 +26,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($_FILES['cover_image']['name'])) {
         $cover = time() . "_cover_" . basename($_FILES["cover_image"]["name"]);
         move_uploaded_file($_FILES["cover_image"]["tmp_name"], "../uploads/" . $cover);
-        $update_sql = "UPDATE blogs SET title='$title', description='$description', cover_image='$cover' WHERE id=$id";
+        $update_sql = "UPDATE blogs SET title='$title', 
+                   heading='$heading', 
+                   headingbrief='$headingbrief', 
+                   p1='$p1', 
+                   p2='$p2', 
+                   conclusion='$conclusion', cover_image='$cover' WHERE id=$id";
     }
     if (!empty($_FILES['image1']['name'])) {
         $img1 = time() . "_img1_" . basename($_FILES["image1"]["name"]);
         move_uploaded_file($_FILES["image1"]["tmp_name"], "../uploads/" . $img1);
-        $update_sql = "UPDATE blogs SET title='$title', description='$description', image1='$img1' WHERE id=$id";
+        $update_sql = "UPDATE blogs SET title='$title', 
+                   heading='$heading', 
+                   headingbrief='$headingbrief', 
+                   p1='$p1', 
+                   p2='$p2', 
+                   conclusion='$conclusion', image1='$img1' WHERE id=$id";
     }
     if (!empty($_FILES['image2']['name'])) {
         $img2 = time() . "_img2_" . basename($_FILES["image2"]["name"]);
         move_uploaded_file($_FILES["image2"]["tmp_name"], "../uploads/" . $img2);
-        $update_sql = "UPDATE blogs SET title='$title', description='$description', image2='$img2' WHERE id=$id";
+        $update_sql = "UPDATE blogs SET title='$title', 
+                   heading='$heading', 
+                   headingbrief='$headingbrief', 
+                   p1='$p1', 
+                   p2='$p2', 
+                   conclusion='$conclusion', image2='$img2' WHERE id=$id";
     }
 
     mysqli_query($conn, $update_sql);
@@ -45,41 +60,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Edit Blog</title></head>
+
+<head>
+    <title>Edit Blog</title>
+</head>
+
 <body>
-  <h2>Edit Blog</h2>
-  <form method="POST" enctype="multipart/form-data">
-  <label>Title:</label><br>
-    <input type="text" name="title" value="<?= htmlspecialchars($blog['title']); ?>" required><br><br>
+    <h2>Edit Blog</h2>
+    <form method="POST" enctype="multipart/form-data">
+        <label>Title:</label><br>
+        <input type="text" name="title" value="<?= htmlspecialchars($blog['title']); ?>" required><br><br>
 
-    <label>Heading:</label><br>
-    <input type="text" name="heading" value="<?= htmlspecialchars($blog['heading']); ?>" required><br><br>
+        <label>Heading:</label><br>
+        <input type="text" name="heading" value="<?= htmlspecialchars($blog['heading']); ?>" required><br><br>
 
-    <label>Heading Brief:</label><br>
-    <textarea name="headingbrief" required><?= htmlspecialchars($blog['headingbrief']); ?></textarea><br><br>
+        <label>Heading Brief:</label><br>
+        <textarea name="headingbrief" required><?= htmlspecialchars($blog['headingbrief']); ?></textarea><br><br>
 
-    <label>Paragraph 1:</label><br>
-    <textarea name="p1" required><?= htmlspecialchars($blog['p1']); ?></textarea><br><br>
+        <label>Paragraph 1:</label><br>
+        <textarea name="p1" required><?= htmlspecialchars($blog['p1']); ?></textarea><br><br>
 
-    <label>Paragraph 2:</label><br>
-    <textarea name="p2" required><?= htmlspecialchars($blog['p2']); ?></textarea><br><br>
+        <label>Paragraph 2:</label><br>
+        <textarea name="p2" required><?= htmlspecialchars($blog['p2']); ?></textarea><br><br>
 
-    <label>Conclusion:</label><br>
-    <textarea name="conclusion" required><?= htmlspecialchars($blog['conclusion']); ?></textarea><br><br>
+        <label>Conclusion:</label><br>
+        <textarea name="conclusion" required><?= htmlspecialchars($blog['conclusion']); ?></textarea><br><br>
 
-    <label>Cover Image:</label><br>
-    <img src="../uploads/<?= $blog['cover_image']; ?>" width="120"><br>
-    <input type="file" name="cover_image"><br><br>
+        <label>Cover Image:</label><br>
+        <img src="../uploads/<?= $blog['cover_image']; ?>" width="120"><br>
+        <input type="file" name="cover_image"><br><br>
 
-    <label>Image 1:</label><br>
-    <img src="../uploads/<?= $blog['image1']; ?>" width="120"><br>
-    <input type="file" name="image1"><br><br>
+        <label>Image 1:</label><br>
+        <img src="../uploads/<?= $blog['image1']; ?>" width="120"><br>
+        <input type="file" name="image1"><br><br>
 
-    <label>Image 2:</label><br>
-    <img src="../uploads/<?= $blog['image2']; ?>" width="120"><br>
-    <input type="file" name="image2"><br><br>
+        <label>Image 2:</label><br>
+        <img src="../uploads/<?= $blog['image2']; ?>" width="120"><br>
+        <input type="file" name="image2"><br><br>
 
-    <button type="submit">Update</button>
-  </form>
+        <button type="submit">Update</button>
+    </form>
 </body>
+
 </html>
