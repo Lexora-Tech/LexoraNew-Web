6dc71dbadb6bf9cfde8fed97f84e296a57c44911
Praveen Lakshan <praveenlakshan05@gmail.com>
2025-09-28 19:42:12 +0530

    mail send index

--- send_email.php
+++ send_email.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (mail($to, $subject, $body, $headers)) {
         // Redirect after success
-        header("Location: index.html");
+        header("Location: index.php");
         exit();
     } else {
         // Redirect after failure
