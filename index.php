9c3a1fbf2c81aeb90dd51f49851b8a4635208a08
Praveen Lakshan <praveenlakshan05@gmail.com>
2025-09-28 11:02:13 +0530

    new

--- index.php
+++ index.php
@@ -56,107 +56,9 @@
         <!-- scroll progress end -->
 
         <!-- fixed elements -->
-        <div class="mil-fixed">
-            <div class="mil-top-panel-2">
-                <div class="container">
-                    <div class="mil-left-side mil-tp-transition" id="swupTpLeft">
-                        <a href="index.php" class="mil-logo mil-scroll-to" data-no-swup>
-                            <!--  <i class="far fa-cube"></i> -->
-                            <img src="img/logo/logo2.png" width="60px" height="38px">
-                            <span>LexoraTech</span>
-                        </a>
-                    </div>
-
-                    <div class="mil-onepage-nav" id="swup-opm">
-                        <ul>
-                            <li><a href="#services" class="mil-scroll-to" data-no-swup>Services</a></li>
-                            <!-- <li><a href="#team" class="mil-scroll-to" data-no-swup>Team</a></li> -->
-                            <li><a href="#reviews" class="mil-scroll-to" data-no-swup>Reviews</a></li>
-                            <li><a href="blog.php"  data-no-swup>Blog</a></li>
-
-                        </ul>
-                    </div>
-
-                    <div class="mil-buttons-tp-frame mil-c-gone">
-                        <div class="mil-buttons">
-                            <a href="contact.php" class="mil-tp-btn"><i class="fal fa-envelope"></i></a>
-                            <div class="mil-tp-btn">
-                                <div class="mil-menu-btn"><span></span></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mil-menu-frame mil-menu-frame-2">
-                <div class="mil-menu-window">
-                    <div class="container">
-                        <div class="row mil-no-g">
-                            <div class="col-lg-6">
-                                <div class="mil-left-side">
-                                    <div class="mil-menu-part mil-inner-scroll" id="swupMenu">
-                                        <ul class="mil-main-menu mil-c-gone">
-                                            <!--  <li class="mil-has-children">
-                                                <a href="#.">Look</a>
-
-                                            </li> -->
-
-                                            <li class="mil-has-children">
-                                                <a href="#.">Home</a>
-                                                <ul>
-
-                                                    <li><a href="index">Landing</a></li>
-
-                                                </ul>
-                                            </li>
-                                            <li class="mil-has-children">
-                                                <a href="#.">Pages</a>
-                                                <ul>
-                                                    <li><a href="services.php">Services</a></li>
-                                                   <!--  <li><a href="team-stl.php">Team</a></li> -->
-                                                    <li><a href="contact.php">Contact</a></li>
-                                                    <li><a href="blog.php">Blog</a></li>
-                                                    <!-- <li><a href="">Publication</a></li>
-                                                    <li><a href="">Blog</a></li> -->
-
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="mil-bottom-part">
-                                        <p class="mil-text-sm">©2025 LexoraTech. All rights reserved.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="mil-right-side">
-                                    <div class="mil-links-part">
-                                        <ul class="mil-links">
-                                            <li><a href="#.">Privacy policy</a></li>
-                                            <li><a href="#.">Therms and conditions</a></li>
-                                            <li><a href="#.">Careers</a></li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="mil-blog-part">
-
-                                        <div class="mil-menu-social">
-                                            <ul class="social-icons" style="display:flex; justify-content:flex-end; gap:15px; list-style:none; padding:0; margin:0;">
-                                                <li><a href="https://www.facebook.com/people/LexoraTech/61573037507493/" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-facebook-f"></i></a></li>
-                                                <li><a href="https://www.instagram.com/lexora_tech/" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-instagram"></i></a></li>
-                                                <li><a href="https://www.linkedin.com/company/lexoratech" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-linkedin-in"></i></a></li>
-                                                <li><a href="https://www.youtube.com/channel/UCA-NVZ5FieP8uE1nYyfhRSw" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-youtube"></i></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php
+        include "header.php";
+        ?>
         <!-- fixed elements end -->
 
         <!-- page transition -->
@@ -778,62 +680,9 @@
                     <!-- subscribe end -->
 
                     <!-- footer -->
-                    <footer class="mil-footer mil-p-160-160">
-                        <div class="container">
-                            <div class="row mil-no-g">
-                                <div class="col-lg-6 mil-up">
-                                    <a href="contact.php" class="mil-footer-contact mil-mb90">
-                                        <h6 class="mil-footer-link mil-rubber mil-m1">Let’s TALK</h6>
-                                        <span class="mil-stylized-btn mil-c-gone">
-                                            <i class="fal fa-arrow-up"></i>
-                                            <span>Read more</span>
-                                        </span>
-                                    </a>
-                                </div>
-                                <!--     <div class="col-lg-12">
-                                    <div class="mil-footer-menu-frame mil-mb90 mil-up">
-                                        <ul class="mil-footer-menu">
-                                            <li><a href="#." class="mil-c-gone">Home</a></li>
-                                            <li><a href="#." class="mil-c-gone">Services</a></li>
-                                            <li><a href="#." class="mil-c-gone">About</a></li>
-                                            <li><a href="#." class="mil-c-gone">Portfolio</a></li>
-                                            <li><a href="#." class="mil-c-gone">Contact</a></li>
-                                        </ul>
-                                        <ul class="mil-social mil-c-gone">
-                                            <li><a href="#." target="_blank" data-no-swup><i class="far fa-circle"></i></a></li>
-                                            <li><a href="#." target="_blank" data-no-swup><i class="far fa-circle"></i></a></li>
-                                            <li><a href="#." target="_blank" data-no-swup><i class="far fa-circle"></i></a></li>
-                                            <li><a href="#." target="_blank" data-no-swup><i class="far fa-circle"></i></a></li>
-                                            <li><a href="#." target="_blank" data-no-swup><i class="far fa-circle"></i></a></li>
-                                        </ul>
-                                    </div>
-                                </div> -->
-                                <div class="col-lg-6">
-                                    <div class="footer-column" style="flex:1 1 220px; min-width:200px; text-align:right;">
-                                        <h5 style="font-weight:700; margin-bottom:15px; color:#fff; font-family: Georgia, 'Times New Roman', serif;">Follow Us</h5>
-                                        <ul class="social-icons" style="display:flex; justify-content:flex-end; gap:15px; list-style:none; padding:0; margin:0;">
-                                            <li><a href="https://www.facebook.com/people/LexoraTech/61573037507493/" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-facebook-f"></i></a></li>
-                                            <li><a href="https://www.instagram.com/lexora_tech/" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-instagram"></i></a></li>
-                                            <li><a href="https://www.linkedin.com/company/lexoratech" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-linkedin-in"></i></a></li>
-                                            <li><a href="https://www.youtube.com/channel/UCA-NVZ5FieP8uE1nYyfhRSw" target="_blank" style="color:rgba(255,255,255,0.7); font-size:1.2rem; transition:0.3s; text-decoration:none;"><i class="fab fa-youtube"></i></a></li>
-
-
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="col-lg-12">
-                                    <div class="mil-footer-bottom mil-up" style="font-weight: bold;">
-                                        <p class="mil-text-sm">©2025 LexoraTech All Rights Reserved</p>
-
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </footer>
-
-
-
+                    <?php
+                    include "footer.php";
+                    ?>
                     <!-- footer end -->
 
                 </div>
