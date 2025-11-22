da87c8c4cd3994dcdec57e4957b6144b11a200dc
Praveen Lakshan <praveenlakshan05@gmail.com>
2025-11-22 18:13:45 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
  <?php
 $host = 'localhost';
-$db = 'magaqmco_lexoraadmin';
-$user = 'magaqmco_lexora';
-$pass = 'Lexora12345@#';
+$db = 'blog_system';
+$user = 'root';
+$pass = 'JapL050514';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
