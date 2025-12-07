9f73289c295613b93747b33ee668da8cdc87396b
Theekshana <145346863+ThikkaBro@users.noreply.github.com>
2025-12-07 14:50:12 +0530

    updated

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- grid css -->
-    <link rel="stylesheet" href="css/plugins/bootstrap-grid.css">
+    <link rel="stylesheet" href="/css/plugins/bootstrap-grid.css">
     <!-- font awesome css -->
-    <link rel="stylesheet" href="css/plugins/fontawesome.min.css">
+    <link rel="stylesheet" href="/css/plugins/fontawesome.min.css">
     <!-- swiper css -->
-    <link rel="stylesheet" href="css/plugins/swiper.min.css">
+    <link rel="stylesheet" href="/css/plugins/swiper.min.css">
     <!-- okai css -->
-    <link rel="stylesheet" href="css/style-stylish.css">
+    <link rel="stylesheet" href="/css/style-stylish.css">
     <!-- page title -->
     <title>Lexora Tech</title>
     <!-- logo css -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/logo/logo.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="/img/logo/logo.png" />
 
 </head>
 
@@ -683,24 +683,24 @@
     <!-- wrapper end -->
 
     <!-- swup js -->
-    <script src="js/plugins/swup.min.js"></script>
+    <script src="/js/plugins/swup.min.js"></script>
     <!-- gsap js -->
-    <script src="js/plugins/gsap.min.js"></script>
+    <script src="/js/plugins/gsap.min.js"></script>
     <!-- scroll smoother -->
-    <script src="js/plugins/ScrollSmoother.min.js"></script>
+    <script src="/js/plugins/ScrollSmoother.min.js"></script>
     <!-- scroll trigger js -->
-    <script src="js/plugins/ScrollTrigger.min.js"></script>
+    <script src="/js/plugins/ScrollTrigger.min.js"></script>
     <!-- scroll to js -->
-    <script src="js/plugins/ScrollTo.min.js"></script>
+    <script src="/js/plugins/ScrollTo.min.js"></script>
     <!-- swiper js -->
-    <script src="js/plugins/swiper.min.js"></script>
+    <script src="/js/plugins/swiper.min.js"></script>
     <!-- parallax js -->
-    <script src="js/plugins/parallax.js"></script>
+    <script src="/js/plugins/parallax.js"></script>
     <!-- FontAwesome for Icons -->
     <script src="https://kit.fontawesome.com/yourkit.js" crossorigin="anonymous"></script>
 
     <!-- Lexora Tech js -->
-    <script src="js/main.js"></script>
+    <script src="/js/main.js"></script>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3130344952697641"
         crossorigin="anonymous"></script>
 </body>
